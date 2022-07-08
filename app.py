2b5ddff37958152559781ac31e1f0fa464353c7a
z3r0fl4g <z3r0fl4g@protonmail.com>
2022-07-08 12:52:29 -0400

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
